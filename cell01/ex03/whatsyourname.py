d63ead5ab068ba3a167b48119e10f72356df8652
Nattamon06 <68040708@kmitl.ac.th>
2025-07-22 09:02:12 +0000

    whatsyourname.py

---
+++ cell01/ex03/whatsyourname.py
@@ -0,0 +1,3 @@
+first_name = input("Hey, what's your first name? : Nattamon ")
+last_name = input("And your last name? : Sornnuwat")
+print("Well, pleased to meet you, Nattamon Sornnuwat", first_name, last_name)
